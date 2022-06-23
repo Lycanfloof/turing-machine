217bc09fdc884bdf731856fa87e99afd7bc4be75
Lycanfloof <lyca2206@gmail.com>
2022-06-23 00:03:59 -0500

    Minor bug fixing in UI class

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,12 +7,11 @@ import model.TuringMachine;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		TuringMachine turingMachine = new TuringMachine('A', new char[] {'0', '1', '_'});
+		TuringMachine turingMachine = new TuringMachine('A', new char[] { '0', '1', '_' });
 		Scanner scanner = new Scanner(System.in);
 		TuringMachineUI turingMachineUI = new TuringMachineUI(turingMachine, scanner);
-		turingMachineUI.menu();
 		
+		turingMachineUI.menu();
 	}
 
 }
